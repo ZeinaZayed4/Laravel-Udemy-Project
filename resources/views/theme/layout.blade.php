dd48b366271a9127dd9ded7bb994ec3a6d60574b
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-01-09 18:38:19 +0200

    Theme to views

---
+++ resources/views/theme/layout.blade.php
@@ -0,0 +1,12 @@
+@include('theme.partials.head')
+<!--================Header Menu Area =================-->
+@include('theme.partials.header')
+<!--================Header Menu Area =================-->
+
+@yield('content')
+
+<!--================ Start Footer Area =================-->
+@include('theme.partials.footer')
+<!--================ End Footer Area =================-->
+
+@include('theme.partials.scripts')
